6762009c97c3c644f98e565c742350a06be83794
kvscomputing <kvseetharaman2@gmail.com>
2023-02-18 11:51:34 -0800

    committing new screens

---
+++ FrolicFr/CreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateAccountViewController.swift
+//  FrolicFr
+//
+//  Created by Karthik Seetharaman on 2/18/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUp(_ sender: UIButton) {
+        //create the account
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
